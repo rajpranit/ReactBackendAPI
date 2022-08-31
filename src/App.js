b9de9bfd79460c571f1285bd49f2fa5138f50bc9
rajpranit <rajpranit1207@gmail.com>
2022-08-31 11:52:28 +0530

    async / await

--- src/App.js
+++ src/App.js
@@ -5,23 +5,21 @@ import { useState } from "react";
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-  const fetchMovieHandler = () => {
-    fetch("https://swapi.py4e.com/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const TransformedData = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(TransformedData);
-      });
-  };
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.py4e.com/api/films/");
+
+    const data = await response.json();
+
+    const TransformedData = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(TransformedData);
+  }
 
   return (
     <>
