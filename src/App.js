f8635ce0d88a28c81aa594382176e688201fe590
rajpranit <rajpranit1207@gmail.com>
2022-09-03 10:22:51 +0530

    Seding POST request through fetch post method

--- src/App.js
+++ src/App.js
@@ -1,39 +1,67 @@
 import MoviesList from "./Components/MoviesList";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import AddMovie from "./Components/AddMovie";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMovieHandler() {
+  const fetchMovieHandler = useCallback(async () => {
     try {
       setIsLoading(true);
-      const response = await fetch("https://swapi.py4e.com/api/films/");
+      const response = await fetch(
+        "https://react-http-95b9d-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error(response.status.toString());
       }
 
       const data = await response.json();
-      console.log("getting data");
 
-      const TransformedData = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-      });
-      setMovies(TransformedData);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        if (data[key].title != "") {
+          loadedMovies.push({
+            id: key,
+            title: data[key].title,
+            openingText: data[key].openingText,
+            releaseDate: data[key].releaseDate,
+          });
+        }
+      }
+
+      setMovies(loadedMovies);
       setIsLoading(false);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, [movies]);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
+
+  const addMovieHandler = async (movie) => {
+    try {
+      const response = await fetch(
+        "https://react-http-95b9d-default-rtdb.firebaseio.com/movies.json",
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+        }
+      );
+
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   let content = <p>No Movies found</p>;
 
@@ -51,6 +79,9 @@ const App = () => {
 
   return (
     <>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+      </section>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
