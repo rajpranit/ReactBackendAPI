c84f63c6f882a9e678bebae50198a8f58459ae10
rajpranit <rajpranit1207@gmail.com>
2022-09-03 09:09:22 +0530

    Try and Catch possible error

--- src/App.js
+++ src/App.js
@@ -5,24 +5,48 @@ import { useState } from "react";
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMovieHandler() {
-    setIsLoading(true);
-    const response = await fetch("https://swapi.py4e.com/api/films/");
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.py4e.com/api/films/");
 
-    const data = await response.json();
-    console.log("getting data");
+      if (!response.ok) {
+        throw new Error(response.status.toString());
+      }
 
-    const TransformedData = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
-    setMovies(TransformedData);
+      const data = await response.json();
+      console.log("getting data");
+
+      const TransformedData = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(TransformedData);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>No Movies found</p>;
+
+  if (isLoading) {
+    content = <h3>loading...</h3>;
+  }
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error} Something Went Wrong</p>;
   }
 
   return (
@@ -31,8 +55,11 @@ const App = () => {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading ? <MoviesList movies={movies} /> : <h3>loading...</h3>}
-        {!isLoading && movies.length === 0 && <p>No movies found</p>}
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found</p>}
+        {isLoading && error == null && <h3>loading...</h3>}
+        {error &&  <p>{error} Something Went Wrong</p>} */}
+        {content}
       </section>
     </>
   );
